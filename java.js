d4b490d88a7f4d0d32dcd698fe499a430c8db0a6
Weijun Chen <weijunchen@Weijuns-MacBook-Pro.local>
2020-02-25 20:11:59 -0500

    last try

--- java.js
+++ java.js
@@ -6,3 +6,51 @@ function myFunction() {
         x.style.display = "block";
     }
 }
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function rollDropdown() {
+    console.log("in dropdown");
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+
+function ahriDropdown() {
+    console.log("in dropdown");
+    let dropdownElement = document.getElementById("myDropdownA");
+    dropdownElement.classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
